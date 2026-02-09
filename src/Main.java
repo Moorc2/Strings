08e726db990caf4a0dd9af94e44e7431a4d8e402
Charles Moore <moorc2@mail.uc.edu>
2026-02-08 20:54:49 -0500

    Strings project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String user = "Tom";
+        String lastName = "Wulf";
+        String fullName = user + " " + lastName;
+
+        System.out.println(fullName);
+
+        int score = 100;
+
+        System.out.println("The score is " + score);  // "100"
+
+        int tomLength = user.length();  // 3
+
+        System.out.println(tomLength);
+
+        System.out.println(user.toLowerCase()); // "tom"
+        System.out.println(user.toUpperCase()); // "TOM"
+        System.out.println(user);
+
+        user = user.toUpperCase();
+        System.out.println(user);
+
+        String first = fullName.substring(0, 3);
+        System.out.println("First is " + first);
+
+        String second = fullName.substring(4);
+        System.out.println("Second is " + second);
+
+        String bill = "Bill";
+        String billy = bill;
+
+        System.out.println("Tom".equals("TOM"));
+        System.out.println("Tom".equalsIgnoreCase("TOM"));
+
+        String untrimmed = "  an untrimmed String ";
+
+        System.out.println(untrimmed.trim());
+
+        String num100 = String.valueOf(100); // "100"
+        System.out.println(num100);
+
+        //  \n  newline   \t
+
+        System.out.println("Mary had a little lamb!");
+        System.out.println("Mary\t had\t a\t little\t lamb!");
+        System.out.println("Mary\n had\n a\n little\n lamb!");
+
     }
 }
